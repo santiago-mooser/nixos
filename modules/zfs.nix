49cc88aa2867b7235d9b88e5d6f42051dc98a7f2
santiago-mooser <contact@santiago-mooser.com>
2025-07-23 10:53:57 +0800

    fix: changed the key loading to happen using root

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -45,13 +45,14 @@
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
+      User = "root";  # Explicitly run as root to access /root/.keys/
       ExecStart = pkgs.writeScript "zfs-load-key" ''
         #!${pkgs.bash}/bin/bash
 
         # Check if keyfile exists
         if [ -f /root/.keys/zfs.key ]; then
           # Load key for tank pool (adjust dataset name as needed)
-          ${pkgs.zfs}/bin/zfs load-key -a tank < /root/.keys/zfs.key
+          ${pkgs.zfs}/bin/zfs load-key -L file:///root/.keys/zfs.key -a tank
         else
           echo "Warning: ZFS keyfile not found at /root/.keys/zfs.key"
           exit 1
