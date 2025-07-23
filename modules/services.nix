04d59ef731e364436eedd0b7763b1b99d61da746
santiago-mooser <contact@santiago-mooser.com>
2025-07-23 18:26:20 +0800

    feat: added vscode server

--- modules/services.nix
+++ modules/services.nix
@@ -4,29 +4,21 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Enable vscode-server for remote development
   imports = [
     (fetchTarball "https://github.com/nix-community/nixos-vscode-server/tarball/master")
   ];
-  services.vscode-server.enable = true;
 
-  # Install VSCode with FHS environment and dependencies for extensions
+  services.vscode-server.enable = true;
+  services.vscode-server.enableFHS = true;
+
   environment.systemPackages = with pkgs; [
     tailscale
-    (vscode.fhsWithPackages (ps: with ps; [
-      nodejs
-      python3
-      python3Packages.pip
-      python3Packages.isort
-      python3Packages.black
-      git
-    ]))
   ];
+
 
   # Enable Tailscale
   services.tailscale = {
     enable = true;
-    # Allow Tailscale to modify routing tables
     useRoutingFeatures = "both";
   };
 
@@ -42,34 +34,29 @@
     '';
   };
 
-  # Allow Tailscale through the firewall (this will be handled by UFW, but keeping for compatibility)
   networking.firewall = {
     trustedInterfaces = [ "tailscale0" ];
     allowedUDPPorts = [ config.services.tailscale.port ];
   };
 
-  # Enable avahi for local network discovery
   services.avahi = {
     enable = true;
     nssmdns4 = true;
     openFirewall = true;
   };
 
-  # Enable automatic updates for security (optional but recommended)
   system.autoUpgrade = {
     enable = true;
-    allowReboot = false; # Set to true if you want automatic reboots
+    allowReboot = false;
     channel = "https://nixos.org/channels/nixos-24.05";
   };
 
-  # Enable automatic garbage collection
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 30d";
   };
 
-  # Optimize Nix store automatically
   nix.optimise = {
     automatic = true;
     dates = [ "03:45" ];
